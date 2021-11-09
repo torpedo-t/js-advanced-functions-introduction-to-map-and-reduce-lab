b2d463abde06e7ea85078f8c37907d42f4879463
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-11-09 12:34:23 -0600

    commit tree 017f5383b0d6f483943bd97b8d22f6f43918afb1

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+function mapToNegativize(sourceArray){
+    return sourceArray.map(x => x * -1);
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray.map(x => x);
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(x => x*2);
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(x => x*x);
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    const reducer = function(accumulator, currentValue){ return accumulator + currentValue }
+
+    return sourceArray.reduce(reducer, startingPoint)
+}
+
+function reduceToAllTrue(sourceArray){
+
+    const reducer = function(accumulator, currentValue){  
+        if(!!accumulator == true && !!currentValue == true){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    return sourceArray.reduce(reducer, true)
+}
+
+function reduceToAnyTrue(sourceArray){
+    const reducer = function(accumulator, currentValue){  
+        if(accumulator == true){
+            return true
+        } else {
+            return !!currentValue
+        } 
+    }
+    return sourceArray.reduce(reducer, false)
+}
+
+// what is the job of this function? is it supposed to return a string, modify an array?
+
+// each function needs a single, clear purpose...............................................
+
+// what data/info does it need in order to do it's job? what arguments, parameters is necessary?
+
+// what am i expecting as a return value? Should be pretty distinct
